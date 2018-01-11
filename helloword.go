2a690cefd40fb17979de67f4286ac511f477f0c7
Administrator <390289389@qq.com>
2018-01-11 13:54:25 +0800

    first commit

---
+++ helloword.go
@@ -0,0 +1,15 @@
+package main
+import  "fmt"
+
+func main (){
+	var x = 100
+	var y = 122
+	fmt.Println(x)
+	fmt.Println(y)
+	var z = x
+	fmt.Println(z)
+}
+
+
+
+
---
+++ test1.go
@@ -0,0 +1,12 @@
+package main
+import  "fmt"
+
+
+func main ()  {
+	var x  = 1
+	var v int = 2
+	fmt.Println(x)
+	fmt.Println(v)
+
+
+}
